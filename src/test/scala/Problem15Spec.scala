a3aa9feb8b9c790f49aa3522fae71391e09b18eb
Clive Evans <cliveevans@ntlworld.com>
2015-11-30 11:23:53 +0000

    problem 15 statement

---
+++ src/test/scala/Problem15Spec.scala
@@ -0,0 +1,16 @@
+
+class Problem15Spec {
+
+  /*
+  Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+  rrdd
+  rdrd
+  rddr
+  drrd
+  ddrr
+  
+
+  How many such routes are there through a 20×20 grid?
+   */
+}
